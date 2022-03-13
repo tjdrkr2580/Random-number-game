a4007b9b7f00b74bd7bb73b4215884f9fe7f3ca2
Wonsi <tjdrkr2580@naver.com>
2022-03-14 03:44:55 +0900

    already C

---
+++ JS/app.js
@@ -0,0 +1,63 @@
+const timer = document.getElementById("time");
+const start = document.getElementById("start");
+const number = document.getElementById("input_number");
+const submit = document.getElementById("submit");
+const updown = document.getElementById("up_down");
+
+let second = 30;
+let secret_number = 0;
+let GAME_START = false;
+
+function timerCheck() {
+    let time = setInterval(function () {
+        second--;
+        timer.innerText = `TIME: ${second}`;
+        if (second == 0) {
+            stopGame();
+            clearInterval(time);
+        }
+
+    }, 1000)
+}
+
+function gameStart() {
+    GAME_START = true;
+    secret_number = Math.floor(1 + Math.random() * 30);
+    timerCheck();
+    start.disabled = true;
+}
+
+function stopGame() {
+    GAME_START = false;
+    second = 30;
+    start.innerText = `RESTART?`;
+    start.disabled = false;
+}
+
+
+function numberIf(){
+    if(number.value > secret_number){
+        updown.innerText = `DOWN!`;
+    }
+    else if(number.value < secret_number){
+        updown.innerText = `UP!`;
+    }
+}
+
+function gameClear() {
+    updown.innerText = `Correct!`;
+    stopGame();
+    clearInterval(time);
+}
+
+function numberGame() {
+    if (GAME_START == true) {
+        numberIf();
+        if (number.value == secret_number) {
+            gameClear();
+        }
+    }
+}
+
+start.addEventListener("click", gameStart);
+submit.addEventListener("click", numberGame);
